c739a8094bdc28fa0cfc30157ad83d24acf26a55
Todd Fishman <toddfishman@gmail.com>
2025-03-11 20:28:18 -0700

    feat: Working contact search with basic NLP name extraction

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,25 @@
+import 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session {
+    accessToken?: string;
+    refreshToken?: string;
+    error?: string;
+    accessTokenExpires?: number;
+    user: {
+      email: string;
+      name: string;
+      image: string;
+    };
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken?: string;
+    refreshToken?: string;
+    accessTokenExpires?: number;
+    error?: string;
+  }
+}
